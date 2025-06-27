c5bd2b8bae0b3bb21ad627b5f49c315ff37fe0db
Saulo95 <saulgonzalez@Sauls-M1-Laptop-2.local>
2025-06-27 07:20:12 -0600

    Added gotos in main

--- src/uimain.c
+++ src/uimain.c
@@ -13,16 +13,16 @@ int main() {
     printf("Type text to tokenize.\n");
     printf("Use '!n', history, exit as options\n");
 
-    while (1) {
+ start:
         printf("> ");
-        if (fgets(input, sizeof(input), stdin) == NULL) break;
+        if (fgets(input, sizeof(input), stdin) == NULL) goto end;;
 	
         char *end = strchr(input, '\n');
         if (end) *end = '\0';
-        if (strcmp(input, "exit") == 0) break;
+        if (strcmp(input, "exit") == 0) goto end;
         if (strcmp(input, "history") == 0) {
             print_history(history);
-            continue;}
+            goto start;}
 
         if (input[0] == '!' && input[1] != '\0') {
             int id = atoi(&input[1]);
@@ -36,12 +36,14 @@ int main() {
                 }
             } else {
                 printf("Error!! No history for:!%d\n", id);}
-            continue; }
+            goto start; }
         add_history(history, input);
         char **tokens = tokenize(input);
         if (tokens) {
             print_tokens(tokens);
-            free_tokens(tokens); }}
+            free_tokens(tokens); }
+	goto start;
+ end:
 
     free_history(history);
     return 0;}
